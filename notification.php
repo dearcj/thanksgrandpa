a551af682eac5404f9cf462e577b76c8991dabc8
KURWINDALLAS <ddearcj@gmail.com>
2015-02-17 23:58:16 +0200

    0

--- notification.php
+++ notification.php
@@ -1,10 +1,13 @@
 
 <?php
 header("Content-Type: application/json; encoding=utf-8");
+
+try
+{
 $conn = new PDO ("sqlsrv:server = tcp:te1gwbas4s.database.windows.net,1433; Database = thanksdad_db", "crazyjuice",
 "girkinLOH1");
 $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION );
-catch (PDOException $e ) {
+} catch (PDOException $e ) {
  print( "Error connecting to SQL Server." );asd
   die(print_r($e));
   }
