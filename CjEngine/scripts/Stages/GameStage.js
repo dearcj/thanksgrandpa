a5a859085d8004f197b693a5f2f36bfbbd72b69c
KURWINDALLAS <ddearcj@gmail.com>
2015-02-17 19:08:46 +0200

    0

--- CjEngine/scripts/Stages/GameStage.js
+++ CjEngine/scripts/Stages/GameStage.js
@@ -71,7 +71,7 @@ GameStage.prototype.createCircle = function (w, x, y, r) {
 GameStage.prototype.process = function () {
     /*   if (this.doPhys)
      world.step(this.invFR*this._worldSpeed);
-     ddsdd*/
+     ddsd[d*/
     window.focus();
     CObj.processAll();
 
