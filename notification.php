c3b49c8e99aac0e44d217ba83f822d527a49819d
KURWINDALLAS <ddearcj@gmail.com>
2015-02-17 23:58:49 +0200

    0

--- notification.php
+++ notification.php
@@ -8,7 +8,7 @@ $conn = new PDO ("sqlsrv:server = tcp:te1gwbas4s.database.windows.net,1433; Data
 "girkinLOH1");
 $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION );
 } catch (PDOException $e ) {
- print( "Error connecting to SQL Server." );asd
+ print( "Error connecting to SQL Server." );
   die(print_r($e));
   }
   ?>
