9d0b30c77fbf6d9dbb902793f3757ec2e5601756
KURWINDALLAS <ddearcj@gmail.com>
2015-02-18 15:05:37 +0200

    0

--- notification.php
+++ notification.php
@@ -5,7 +5,8 @@ try
 $pdo = new PDO("dblib:host=te1gwbas4s.database.windows.net;dbname=thanksdad_db",
                 "crazyjuice","girkinLOH1");
 $q = $pdo->query('SELECT id, vkapi FROM tb_players');
-echo($q);
+print($q[0]);
+
 foreach($q as $row) {
                         echo($row);
                     }
