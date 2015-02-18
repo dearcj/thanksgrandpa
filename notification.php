f6cb7aeaca43801ed5595b8147a2ee866588873d
KURWINDALLAS <ddearcj@gmail.com>
2015-02-18 15:10:33 +0200

    0

--- notification.php
+++ notification.php
@@ -5,7 +5,7 @@ try
 $pdo = new PDO("dblib:host=te1gwbas4s.database.windows.net;dbname=thanksdad_db",
                 "crazyjuice","girkinLOH1");
 
-$statement = $conn->prepare("SELECT id, vkapi FROM thanksdad_db.tb_players");
+$statement = $pdo->prepare("SELECT id, vkapi FROM thanksdad_db.tb_players");
     $result = $statement->execute();
 
 $q = $pdo->query('SELECT id, vkapi FROM thanksdad_db.tb_players');
