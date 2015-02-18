3cbc8c33d5533c93ef83ea23bd68f571c905af61
KURWINDALLAS <ddearcj@gmail.com>
2015-02-18 03:43:54 +0200

    0

--- notification.php
+++ notification.php
@@ -1,7 +1,7 @@
 
 <?php
 try
-{new PDO ("sqlsrv:server = tcp:te1gwbas4s.database.windows.net,1433;database = thanksdad_db", "crazyjuice",
+{new PDO ("sqlsrv:server=tcp:te1gwbas4s.database.windows.net,1433;database=thanksdad_db", "crazyjuice",
 "girkinLOH1");
 $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION );
 }
