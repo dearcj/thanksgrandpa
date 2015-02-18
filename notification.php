61e1ca1fa766d06f31e060e4de756da8e8dbfd28
KURWINDALLAS <ddearcj@gmail.com>
2015-02-18 15:00:17 +0200

    0

--- notification.php
+++ notification.php
@@ -4,11 +4,12 @@ try
 {
 $pdo = new PDO("dblib:host=te1gwbas4s.database.windows.net;dbname=thanksdad_db",
                 "crazyjuice","girkinLOH1");
-                 foreach($pdo->query('SELECT * from tb_players') as $row) {
+foreach($pdo->query('SELECT * from tb_players') as $row) {
                         echo($row);
                     }
 }
 catch (PDOException $e) {
+    echo("ASSSSS");
     print "Error!: " . $e->getMessage() . "<br/>";
     die();
 }
