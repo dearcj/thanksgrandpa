44ea5cb25e8dbb4210cf6097ba06abebfe386e10
KURWINDALLAS <ddearcj@gmail.com>
2015-02-18 03:07:23 +0200

    0

--- notification.php
+++ notification.php
@@ -1,18 +1,9 @@
 
 <?php
-header("Content-Type: application/json; encoding=utf-8");
 
-$conn = mssql_connect("te1gwbas4s.database.windows.net", "crazyjuice", "girkinLOH1");
-
-if($conn != FALSE)
-{
-echo "Connected to the database server OK<br />";
-}
-else
-{
-echo "ASSS";
-die("Couldn't connect");
-}
+$connectionInfo = array("UID" => "crazyjuice@te1gwbas4s", "pwd" => "girkinLOH1", "Database" => "thanksdad_db", "LoginTimeout" => 30, "Encrypt" => 1);
+$serverName = "tcp:te1gwbas4s.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
 
   ?>
 
