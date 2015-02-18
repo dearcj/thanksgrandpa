73f171c7b9c74bb7f763381cacdbd4c845a7ef28
KURWINDALLAS <ddearcj@gmail.com>
2015-02-18 15:22:39 +0200

    0

--- notification.php
+++ notification.php
@@ -6,8 +6,6 @@ $pdo = new PDO("dblib:host=te1gwbas4s.database.windows.net;dbname=thanksdad_db",
                 "crazyjuice","girkinLOH1");
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $statement = $pdo->prepare("select id from crazyjuice.tb_players");
-$result = $statement->execute();
-$row = $statement->fetch();
 print $row;
 }
 catch (PDOException $e) {
@@ -17,3 +15,5 @@ catch (PDOException $e) {
 }
   ?>
 
+//$result = $statement->execute();
+$row = $statement->fetch();
