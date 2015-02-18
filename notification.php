b5748182c08172531800317e8046ca15652f6966
KURWINDALLAS <ddearcj@gmail.com>
2015-02-18 15:17:33 +0200

    0

--- notification.php
+++ notification.php
@@ -5,7 +5,10 @@ try
 $pdo = new PDO("dblib:host=te1gwbas4s.database.windows.net;dbname=thanksdad_db",
                 "crazyjuice","girkinLOH1");
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
+$statement = $pdo->prepare("SELECT * FROM tb_players");
+$result = $statement->execute();
+$row = $statement->fetch();
+print $row;
 }
 catch (PDOException $e) {
     echo("ASSSSS");
