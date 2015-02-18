104e90af99476f13509293ed89dc44e00ff45d84
KURWINDALLAS <ddearcj@gmail.com>
2015-02-18 02:41:19 +0200

    0

--- notification.php
+++ notification.php
@@ -2,12 +2,16 @@
 <?php
 header("Content-Type: application/json; encoding=utf-8");
 
-try
-{
 $conn = mssql_connect("tcp:te1gwbas4s.database.windows.net,1433", "crazyjuice", "girkinLOH1");
-} catch (PDOException $e ) {
- print( "Error connecting to SQL Server." );
-  die(print_r($e));
-  }
+
+if($connection != FALSE)
+{
+echo "Connected to the database server OK<br />";
+}
+else
+{
+die("Couldn't connect");
+}
+
   ?>
 
