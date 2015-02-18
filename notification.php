111b42d9126298b89525ee6801db463a4cedcb9d
KURWINDALLAS <ddearcj@gmail.com>
2015-02-18 02:44:05 +0200

    0

--- notification.php
+++ notification.php
@@ -4,7 +4,7 @@ header("Content-Type: application/json; encoding=utf-8");
 
 $conn = mssql_connect("tcp:te1gwbas4s.database.windows.net,1433", "crazyjuice", "girkinLOH1");
 
-if($connection != FALSE)
+if($conn != FALSE)
 {
 echo "Connected to the database server OK<br />";
 }
