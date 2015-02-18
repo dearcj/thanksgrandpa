a3ab419f5e25d5ed668b45a51d6584b4488ecf38
KURWINDALLAS <ddearcj@gmail.com>
2015-02-18 15:21:48 +0200

    0

--- notification.php
+++ notification.php
@@ -5,7 +5,7 @@ try
 $pdo = new PDO("dblib:host=te1gwbas4s.database.windows.net;dbname=thanksdad_db",
                 "crazyjuice","girkinLOH1");
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$statement = $pdo->prepare("select p.id, p.vkapi from dbo.tb_players p");
+$statement = $pdo->prepare("select id from crazyjuice.tb_players");
 $result = $statement->execute();
 $row = $statement->fetch();
 print $row;
