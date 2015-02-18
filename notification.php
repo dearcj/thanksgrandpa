b5b2d0ddb24f6d0a83991ea33ac9cb08214ae2e2
KURWINDALLAS <ddearcj@gmail.com>
2015-02-18 19:36:20 +0200

    0

--- notification.php
+++ notification.php
@@ -38,8 +38,7 @@ $result = $statement->fetchAll();
 print($ids);
 
 $VK = new vkapi($api_id, $secret_key);
-//'access_token'=>$token["access_token"],
-$resp = $VK->api('secure.sendNotification', array('uid'=>'282617259', 'message'=>$msg));
+$resp = $VK->api('secure.sendNotification', array('access_token'=>$token["access_token"], 'uid'=>'282617259', 'message'=>$msg));
 $answer = json_encode($resp);
 print($answer);
 //print_r ($result);
