df50daa04036db5a54051e11680102e5b0e5171b
KURWINDALLAS <ddearcj@gmail.com>
2015-02-18 15:12:44 +0200

    0

--- notification.php
+++ notification.php
@@ -6,8 +6,8 @@ $pdo = new PDO("dblib:host=te1gwbas4s.database.windows.net;dbname=thanksdad_db",
                 "crazyjuice","girkinLOH1");
 
 $statement = $pdo->prepare("SELECT id, vkapi FROM tb_players");
-    $result = $statement->execute();
-
+$result = $statement->execute();
+print_r($result);
 $q = $pdo->query('SELECT id, vkapi FROM tb_players');
 
 foreach($result as $row) {
