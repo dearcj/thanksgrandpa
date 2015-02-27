43776215f3f02e5e75d412b0da07df42033f3f3e
KURWINDALLAS <ddearcj@gmail.com>
2015-02-27 13:41:40 +0200

    0

--- notification.php
+++ notification.php
@@ -7,8 +7,8 @@ try
 $api_id = 4654201;
 $secret_key = 'qV6RXByT51TBnwGZX8Py'; // Защищенный ключ приложения
 
-$api_id = 3824852;
-$secret_key = 'CIoXBH0UcOkIhrEiR9rt'; // Защищенный ключ приложения
+//$api_id = 3824852;
+//$secret_key = 'CIoXBH0UcOkIhrEiR9rt'; // Защищенный ключ приложения
 
 $params = array(
 	        'client_id' => $api_id,
