0e1a4db34119abb6c8f125da0a9b621d337576b8
KURWINDALLAS <ddearcj@gmail.com>
2015-03-23 15:31:21 +0200

    0

--- upload.php
+++ upload.php
@@ -4,7 +4,7 @@ require_once 'Zend/Http/Client.php';
 $input = $_POST;
 
 $url = $_POST['uploadUrl'];
-//$url = "http://cs623321.vk.com/upload.php?act=do_add&mid=282617259&aid=-14&gid=0&hash=056af4e8fb95b8bc22e91e243fb44d04&rhash=8f1395a8705d5ebf6ee91fe79c70b6a3&swfupload=1&api=1&wallphoto=1";
+$url = "http://cs623321.vk.com/upload.php?act=do_add&mid=282617259&aid=-14&gid=0&hash=056af4e8fb95b8bc22e91e243fb44d04&rhash=8f1395a8705d5ebf6ee91fe79c70b6a3&swfupload=1&api=1&wallphoto=1";
 $photo = $_POST['photo'];
 //if (isset($_POST['uploadUrl'])) {
 // создаем объект клиента
