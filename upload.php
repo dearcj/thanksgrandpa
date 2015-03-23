57f1d6f639da827d97f3ff5fee30650134f70aa1
KURWINDALLAS <ddearcj@gmail.com>
2015-03-23 16:21:55 +0200

    0

--- upload.php
+++ upload.php
@@ -3,6 +3,7 @@ header("Content-Type: application/json; encoding=utf-8");
 require_once 'Zend/Http/Client.php';
 
 $url = $_POST['uploadUrl'];
+$url = "http://cs623321.vk.com/upload.php?act=do_add&mid=282617259&aid=-14&gid=0&hash=056af4e8fb95b8bc22e91e243fb44d04&rhash=8f1395a8705d5ebf6ee91fe79c70b6a3&swfupload=1&api=1&wallphoto=1"
 //$url = "http://cs623321.vk.com/upload.php?act=do_add&mid=282617259&aid=-14&gid=0&hash=056af4e8fb95b8bc22e91e243fb44d04&rhash=8f1395a8705d5ebf6ee91fe79c70b6a3&swfupload=1&api=1&wallphoto=1";
 $photo = $_POST['photo'];
 
@@ -10,11 +11,11 @@ $client = new Zend_Http_Client($url);
 $client->setMethod(Zend_Http_Client::POST);
 $photodecoded = base64_decode($photo);
 //echo $photodecoded;
-$client->setParameterPost('photo', $photodecoded);
+//$client->setParameterPost('photo', $photodecoded);
 //$imagePathname = 'http://www.dedgame.ru/logo_ingame.png';
 //$client->setFileUpload($imagePathname, 'photo');
-//$client->setFileUpload(NULL, 'photo', $photo, 'text/plain');
-$client->setRawData($photodecoded, 'text/xml')->request('POST');
+$client->setFileUpload(NULL, 'photo', $photo, 'text/plain');
+//$client->setRawData($photodecoded, 'text/xml')->request('POST');
 
 $result = $client->request();
 echo $result->getBody();
