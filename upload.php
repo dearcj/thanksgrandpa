bbbebbd60b3c663893445c6832959c962cdaf3d5
KURWINDALLAS <ddearcj@gmail.com>
2015-03-23 14:21:58 +0200

    0

--- upload.php
+++ upload.php
@@ -8,13 +8,14 @@ $input = $_POST;
 
 $url = $_POST['uploadUrl'];
 $url = "http://cs623321.vk.com/upload.php?act=do_add&mid=282617259&aid=-14&gid=0&hash=056af4e8fb95b8bc22e91e243fb44d04&rhash=8f1395a8705d5ebf6ee91fe79c70b6a3&swfupload=1&api=1&wallphoto=1";
-
+$photo = $_POST['photo'];
 //if (isset($_POST['uploadUrl'])) {
 // создаем объект клиента
 $client = new Zend_Http_Client($url);
 $client->setMethod(Zend_Http_Client::POST);
-$imagePathname = 'logo_ingame.jpg';
-$client->setFileUpload($imagePathname, 'photo');
+$client->setParameterPost('photo', $photo);
+//$imagePathname = 'logo_ingame.jpg';
+//$client->setFileUpload($imagePathname, 'photo');
 $result = $client->request();
 if ($result->isError()) {
 echo $result->getStatus();
