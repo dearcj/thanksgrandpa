b9c97fa3ac4bee4654bedddb1b8f5573d6a4c181
KURWINDALLAS <ddearcj@gmail.com>
2015-03-23 17:10:38 +0200

    0

--- upload.php
+++ upload.php
@@ -12,9 +12,10 @@ $client = new Zend_Http_Client($url);
 $client->setMethod(Zend_Http_Client::POST);
 $photodecoded = base64_decode($photo);
 //echo $photodecoded;
-$client->setParameterPost('photo', $photo);
+//$client->setParameterPost('photo', $photo);
 //$imagePathname = 'http://www.dedgame.ru/logo_ingame.png';
 //$client->setFileUpload($imagePathname, 'photo');
+echo $photodecoded;
 $client->setFileUpload("15x15.png", 'photo', $photodecoded, 'image/png');
 //$client->setRawData($photodecoded, 'text/xml')->request('POST');
 
