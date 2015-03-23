7146cf61d0a433d891279ab674698e41de1d0e53
KURWINDALLAS <ddearcj@gmail.com>
2015-03-23 16:24:20 +0200

    0

--- upload.php
+++ upload.php
@@ -5,7 +5,8 @@ require_once 'Zend/Http/Client.php';
 $url = $_POST['uploadUrl'];
 $url = "http://cs623321.vk.com/upload.php?act=do_add&mid=282617259&aid=-14&gid=0&hash=056af4e8fb95b8bc22e91e243fb44d04&rhash=8f1395a8705d5ebf6ee91fe79c70b6a3&swfupload=1&api=1&wallphoto=1"
 //$url = "http://cs623321.vk.com/upload.php?act=do_add&mid=282617259&aid=-14&gid=0&hash=056af4e8fb95b8bc22e91e243fb44d04&rhash=8f1395a8705d5ebf6ee91fe79c70b6a3&swfupload=1&api=1&wallphoto=1";
-$photo = $_POST['photo'];
+//$photo = $_POST['photo'];
+$photo = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAALElEQVQYV2MMypzx/9ev3wyEAKNrbP9/QopA8ow+yZP///z1h6DaUYV4gwgApIAw/Hpi6kkAAAAASUVORK5CYII=";
 
 $client = new Zend_Http_Client($url);
 $client->setMethod(Zend_Http_Client::POST);
