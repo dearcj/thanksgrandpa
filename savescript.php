06d67bcd6e11c6246a107ea23d6c670bf5743432
KURWINDALLAS <ddearcj@gmail.com>
2015-04-14 21:04:18 +0300

    gf

--- savescript.php
+++ savescript.php
@@ -5,7 +5,7 @@ try
 $pl = $_POST['player'];
 
 if (!$pl)
-$pl = json_decode('{"id":"3EA0232F-4A28-4FD2-83AC-EC6254F90BC6","ref":null,"vkapi":"PENNIS","xp":0,"createDate":"2015-04-14T11:36:14.849Z","updateDate":"2015-04-14T15:22:38.462Z","userId":"Custom:3EA0232F-4A28-4FD2-83AC-EC6254F90BC6","money":0,"crystals":0,"maxdistance":153,"lvl":1,"energy":5.086056666666667,"name":null,"last_name":null,"rank":null,"combodate":"2015-04-14T15:21:33.158Z","keys":null,"platformid":"CARLAPA2882845ww"}', true);
+$pl = json_decode('{"id":"3EA0232F-4A28-4FD2-83AC-EC6254F90BC6","ref":null,"vkapi":666,"xp":0,"createDate":"2015-04-14T11:36:14.849Z","updateDate":"2015-04-14T15:22:38.462Z","userId":"Custom:3EA0232F-4A28-4FD2-83AC-EC6254F90BC6","money":0,"crystals":0,"maxdistance":153,"lvl":1,"energy":5.086056666666667,"name":null,"last_name":null,"rank":null,"combodate":"2015-04-14T15:21:33.158Z","keys":null,"platformid":"CARLAPA2882845ww"}', true);
 
 $pdo = new PDO("dblib:host=te1gwbas4s.database.windows.net;dbname=thanksdad_db2",
                 "crazyjuice","girkinLOH1");
