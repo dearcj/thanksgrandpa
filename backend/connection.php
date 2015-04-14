a0c2a1255c975eb34da1730e3099eab0afbc77ed
KURWINDALLAS <ddearcj@gmail.com>
2015-04-15 01:25:38 +0300

    gf

---
+++ backend/connection.php
@@ -0,0 +1,15 @@
+﻿<?php
+try
+{
+
+$secret_key = '56rZjlj9S2O0aUVE';
+$pdo = new PDO("dblib:host=te1gwbas4s.database.windows.net;dbname=thanksdad_db2",
+                "crazyjuice","girkinLOH1", array(PDO::ATTR_PERSISTENT => true));
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+
+    print "Error!: " . $e->getMessage() . "<br/>";
+    die();
+}
+?>
---
+++ backend/login.php
@@ -0,0 +1,29 @@
+﻿<?php
+require 'jwt_helper.php';
+require 'connection.php';
+try
+{
+$vkid = $_POST['vkid'];
+$vkid = '2882845';
+if (!$vkid) return;
+
+$statement = $pdo->prepare("select TOP 1 platformid, id from thanksdad.tb_players WHERE platformid = " . $pdo->quote($vkid));
+$statement->execute();
+$result = $statement->fetchAll();
+   if (count($result > 0))
+   {
+	   $userid = $result[0]['id'];
+   }  
+
+$token = array();
+$token['vkid'] = $vkid;
+$token['userid'] = $userid;
+echo JWT::encode($token, 'secret_server_key');
+
+}
+catch (PDOException $e) {
+
+    print "Error!: " . $e->getMessage() . "<br/>";
+    die();
+}
+?>
