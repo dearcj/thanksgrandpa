fa2f19b3f3a950fe1517f8b162f6d67cbaf9e1d9
KURWINDALLAS <ddearcj@gmail.com>
2015-04-15 11:50:16 +0300

    gf

--- backend/connection.php
+++ backend/connection.php
@@ -4,7 +4,7 @@ try
 
 $secret_key = '56rZjlj9S2O0aUVE';
 $pdo = new PDO("dblib:host=te1gwbas4s.database.windows.net;dbname=thanksdad_db2",
-                "crazyjuice","girkinLOH1"));
+                "crazyjuice","girkinLOH1");
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $pdo->setAttribute(PDO::ATTR_PERSISTENT, true);
 }
