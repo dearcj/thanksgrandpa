95df7a010d673f42d9f448efa5524a71be3e1cd3
KURWINDALLAS <ddearcj@gmail.com>
2015-04-15 12:22:09 +0300

    gf

--- backend/login.php
+++ backend/login.php
@@ -5,7 +5,7 @@ require 'savescript.php';
 try
 {
 $vkid = $_POST['vkid'];
-$vkid = '2882845abbu';
+$vkid = '2882845abbuZ';
 if (!$vkid) return;
 
 $statement = $pdo->prepare("select TOP 1 platformid, id from thanksdad.tb_players WHERE platformid = " . $pdo->quote($vkid));
