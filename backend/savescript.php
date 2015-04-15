6466eceb6a70be0af2d057d66fb858c7e80aefd5
KURWINDALLAS <ddearcj@gmail.com>
2015-04-15 12:44:27 +0300

    gf

--- backend/savescript.php
+++ backend/savescript.php
@@ -19,6 +19,9 @@ $keystr = implode(",",$sqlkeys);
 $wholestr = "INSERT INTO thanksdad.".$table." (".$keystr.") VALUES (".$valuesstr .");";
 $statement = $conn->prepare($wholestr);
 $statement->execute();
+$res = $statement->fetch(PDO::FETCH_ASSOC);
+var_dump($res);
+print $conn->lastInsertId();
 $statement = null;
 return $conn->lastInsertId();
 }
