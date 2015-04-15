b2add3a28a95d2a7301576a872806f9f58500fc5
KURWINDALLAS <ddearcj@gmail.com>
2015-04-15 23:48:57 +0300

    gf

--- backend/login.php
+++ backend/login.php
@@ -9,19 +9,19 @@ $vkid = '2882845';
 $strFindPlayer = "select * from thanksdad.tb_players WHERE platformid = " . $pdo->quote($vkid);
 $statement = $pdo->prepare($strFindPlayer);
 
-	$pdo->beginTransaction();
-	$statement->execute();
-	$result = $statement->fetchAll();
-	if ($result && count($result > 0))
-	{
-		$userid = $result[0]['id'];
-		$playerItem = $result[0];
-		echo 'LOGIN USER WITH ID = ' . $userid;
-	}  else
-	{
-		echo 'REGISTER NEW USER';
-		try
-		{
+$pdo->beginTransaction();
+$statement->execute();
+$result = $statement->fetchAll();
+if ($result && count($result > 0))
+{
+	$userid = $result[0]['id'];
+	$playerItem = $result[0];
+	echo 'LOGIN USER WITH ID = ' . $userid;
+}  else
+{
+	try{
+	echo 'REGISTER NEW USER';
+	
 		$res= insertJSON($pdo, "tb_players", null, array('platformid' => $vkid,
 		'money' => 0,
 		'crystals' => 0,
@@ -48,26 +48,25 @@ $statement = $pdo->prepare($strFindPlayer);
 				
 				insertJSON($pdo, "tb_ach_player", null, $plach);
 			} 
-		}
 		} else throw new Exception('Cant add player record');
 	$pdo->commit();
-	}
-	catch (PDOException $e) {
+}
+catch (PDOException $e) {
 	$pdo->rollBack();
 	print "Error!: " . $e->getMessage() . "<br/>";
 	die();
 }
-	
 
-	if (!$userid) throw new Exception('No user id');
-	$token = array(
-	'vkid' => $vkid,
-	'userid' => $userid
-	);
 
-	$tokenJWT = JWT::encode($token, $secret_key);
-	$response = array('tokenJWT' => $tokenJWT, 'playerItem' => $playerItem);
-	echo json_encode($response);
+if (!$userid) throw new Exception('No user id');
+$token = array(
+'vkid' => $vkid,
+'userid' => $userid
+);
+
+$tokenJWT = JWT::encode($token, $secret_key);
+$response = array('tokenJWT' => $tokenJWT, 'playerItem' => $playerItem);
+echo json_encode($response);
 
 
 ?>
