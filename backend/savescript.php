3c431faddcd65503db457ee367b859599df5e129
KURWINDALLAS <ddearcj@gmail.com>
2015-04-17 00:29:28 +0300

    a

--- backend/savescript.php
+++ backend/savescript.php
@@ -178,8 +178,9 @@ function updateJSON($conn, $table, $jsonEncoded, $userid, $id)
 	
 	foreach($obj as $key => $value){
 		if ($value == '') continue;
+		
 		//$value = mb_convert_encoding($value, 'UCS-2LE', mb_detect_encoding($value, mb_detect_order(), true));
-		$sql[] = (is_numeric($value)) ? "$key = $value" : "$key = " . $conn->quote($value); 
+		$sql[] = (is_numeric($value)) ? "$key = $value" : "$key = N " . $conn->quote($value); 
 	}
 	$f = playerFilter($conn, $table, $userid, $id);
 $sqlclause = implode(",",$sql);
