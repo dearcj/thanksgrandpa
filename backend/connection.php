adb5f7553049d480d9e3cc9c705d2678f9a34111
KURWINDALLAS <ddearcj@gmail.com>
2015-04-16 19:09:29 +0300

    gf

--- backend/connection.php
+++ backend/connection.php
@@ -11,6 +11,7 @@ $pdo = new PDO("dblib:host=e5906u8eu8.database.windows.net;dbname=thanksdad_db4"
 //)
 
 
+$pdo->setAttribute(PDO::ATTR_STRINGIFY_FETCHES, false);
 $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
