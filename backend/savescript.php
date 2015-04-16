98a062eeda01eadf75bbd1fd658d8708957548ad
KURWINDALLAS <ddearcj@gmail.com>
2015-04-16 13:14:13 +0300

    gf

--- backend/savescript.php
+++ backend/savescript.php
@@ -103,8 +103,7 @@ function updateJSON($table, $jsonString, $jsonEncoded,$userid, $id)
 	$f = playerFilter($table, $userid, $id);
 $sqlclause = implode(",",$sql);
 $wholestr = "UPDATE thanksdad.".$table." SET $sqlclause ".$f;
-ini_set($wholestr, "/tmp/php-error.log");
-
+error_log($wholestr, 3 ,"/var/tmp/my-errors.log");
 $statement = $conn->prepare($wholestr);
 $statement->execute();
 $result = $statement->fetchAll(PDO::FETCH_ASSOC);
