92da4df3285e739bbe170c43ce608b217b50fd05
KURWINDALLAS <ddearcj@gmail.com>
2015-04-16 04:00:19 +0300

    gf

--- backend/connection.php
+++ backend/connection.php
@@ -9,8 +9,8 @@ $pdo = new PDO("dblib:host=e5906u8eu8.database.windows.net;dbname=thanksdad_db3"
     PDO::ATTR_PERSISTENT => true
 ));
 
-$pdo->exec('SET CHARACTER SET utf8');
-$pdo->query("SET NAMES utf8");
+$pdo->exec('SET CHARACTER SET windows-1251');
+$pdo->query("SET NAMES windows-1251");
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 }
