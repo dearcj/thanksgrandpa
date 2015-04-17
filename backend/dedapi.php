3f249033374a46c97d544459a3f2e5b48521e0c4
KURWINDALLAS <ddearcj@gmail.com>
2015-04-17 16:38:52 +0300

    a

--- backend/dedapi.php
+++ backend/dedapi.php
@@ -9,9 +9,11 @@ $data = $_POST['data'];
 $table = $_POST['table'];
 $id = $_POST['id'];
 
+/*
 $res = readJSON($pdo, "tb_items", 'F800A350-EE15-4F1C-9CCF-050B775A4CD9', '68AFAEDC-B3E0-401E-9E1A-E272084F2E11');
 var_dump($res);
-echo $res;
+echo $res;*/
+
 /*
 $data = json_decode('{"id":"78F6F7CE-28E3-4AEA-B143-2BBB2CBEA54D","__createdAt":"2015-03-31 09:46:47.464 +00:00","__updatedAt":"2015-04-16 23:25:09.023 +00:00","__deleted":"1","ref":"2882845","vkapi":"2882845","xp":959.39351540461,"createDate":"2015-03-31 09:46:47.034 +00:00","updateDate":"Fri Apr 17 2015 02:30:22 GMT+0300 (FLE Daylight Time)","userId":"Custom:78F6F7CE-28E3-4AEA-B143-2BBB2CBEA54D","money":1500,"crystals":378,"maxdistance":916,"lvl":7,"energy":10,"name":"Юрий","last_name":"Дорогой","rank":1499,"combodate":"2015-03-31 09:54:27.233 +00:00","keys":null,"platformid":"2882845"}');
 $table = 'tb_players';
@@ -36,11 +38,11 @@ $jwtToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ2a2lkIjoiMjg4Mjg0NSIsInVzZX
 */
 
 
-/*
+
 $data = array('id' => '20D318C0-4633-41D0-B406-FD34485A88BE', 'equipped' => true);
 $method = 'BUY_ITEM';
 $jwtToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ2a2lkIjoiMjg4Mjg0NSIsInVzZXJpZCI6Ijc4RjZGN0NFLTI4RTMtNEFFQS1CMTQzLTJCQkIyQ0JFQTU0RCJ9.3e2eO6RCwDPodk2no56Exd8US24dH5XocIR2ZGx-lDk";
-*/
+
 
 if (!$jwtToken) die();
 $token = JWT::decode($jwtToken, $secret_key);
