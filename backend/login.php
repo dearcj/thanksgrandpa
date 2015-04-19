d2cd7b8a88a3889a6f24faef82c86c5548b7eb1a
KURWINDALLAS <ddearcj@gmail.com>
2015-04-19 22:40:47 +0300

    a

--- backend/login.php
+++ backend/login.php
@@ -1,13 +1,10 @@
 ﻿<?php
 header('Content-type: text/json; charset=UTF-8');
-require_once 'jwt_helper.php';
 require_once 'connection.php';
-require_once 'savescript.php';
-require_once 'json.php';
 require_once 'login_api.php';
 
 $vkid = $_POST['vkid'];
-if ($argv[0]) $vkid = $argv[0];
+//if ($argv[0]) $vkid = $argv[0];
 $resp = doLogin($vkid, $pdo, $secret_key);
 $str = json_encode($resp, JSON_UNESCAPED_UNICODE);
 echo $str;
