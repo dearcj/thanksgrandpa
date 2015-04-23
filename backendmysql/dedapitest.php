f1c7696796faeded44f413468ac90f299ba1114d
KURWINDALLAS <ddearcj@gmail.com>
2015-04-23 15:10:40 +0300

    a

--- backendmysql/dedapitest.php
+++ backendmysql/dedapitest.php
@@ -14,7 +14,7 @@ $res = readJSON($pdo, "tb_items", 'F800A350-EE15-4F1C-9CCF-050B775A4CD9', '68AFA
 var_dump($res);
 echo $res;
 */
-
+/*
 $res = insertJSON($pdo, "tb_players", array('platformid' => $vkid,
 			'money' => 0,
 			'crystals' => 0,
@@ -24,15 +24,15 @@ $res = insertJSON($pdo, "tb_players", array('platformid' => $vkid,
 			'maxdistance' => 0));
 			var_dump($res);
 echo $res;
+*/
 
 
-/*
-$data = json_decode('{"id":"F800A350-EE15-4F1C-9CCF-050B775A4CD9","__createdAt":"2015-03-25+12:31:46.510++00:00","__updatedAt":"2015-04-19+23:35:54.506++00:00","__deleted":"1","ref":"282617259","vkapi":"282617259","xp":256.9404239766083,"createDate":"2015-03-25+12:31:46.426++00:00","updateDate":"2015-04-17+00:50:13.000++00:00","userId":"Custom:F800A350-EE15-4F1C-9CCF-050B775A4CD9","money":9639,"crystals":1000,"maxdistance":918,"lvl":3,"energy":10,"name":"Геннадий","last_name":"Геннадич","rank":2467,"combodate":"2015-03-25+15:56:09.522++00:00","keys":null,"platformid":"282617259"}');
+$data = json_decode('{"id":"F800A350-EE15-4F1C-9CCF-050B775A4CD9","ref":"282617259","vkapi":"282617259","xp":256.9404239766083","userId":"Custom:F800A350-EE15-4F1C-9CCF-050B775A4CD9","money":9639,"crystals":1000,"maxdistance":918,"lvl":3,"energy":10,"name":"Геннадий","last_name":"Геннадич","rank":2467,"keys":null,"platformid":"282617259"}');
 $table = 'tb_players';
 $method = 'UPDATE';
 //$id  = '78F6F7CE-28E3-4AEA-B143-2BBB2CBEA54D';
 $jwtToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ2a2lkIjoiMjg4Mjg0NSIsInVzZXJpZCI6Ijc4RjZGN0NFLTI4RTMtNEFFQS1CMTQzLTJCQkIyQ0JFQTU0RCJ9.3e2eO6RCwDPodk2no56Exd8US24dH5XocIR2ZGx-lDk";
-*/
+
 
 /*
 $data = '1000';
