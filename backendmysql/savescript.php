3f92b73f39495709c77539ba271300191e954e7c
KURWINDALLAS <ddearcj@gmail.com>
2015-04-27 11:45:05 +0300

    a

--- backendmysql/savescript.php
+++ backendmysql/savescript.php
@@ -71,7 +71,7 @@ function updateRunProgress($conn, $data, $userid)
 	if ($prevdate)
 	{
 		$since_start = $prevdate->diff($date_curr);
-		var_dump $since_start;
+		var_dump($since_start);
 		echo $since_start->s;
 		
 		if ($since_start->s < $deltasec)
