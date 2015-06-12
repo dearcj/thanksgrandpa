f297ec6bf267dbf2f30fda257282198bff4281df
KURWINDALLAS <ddearcj@gmail.com>
2015-06-12 12:36:36 +0300

    uf

---
+++ backendmysql/yozh_connection.php
@@ -0,0 +1,29 @@
+﻿<?php
+
+
+/*
+$pdo = new PDO('mysql:host=127.0.0.1;dbname=thanksdad', "root", "girkinLOH1", array(
+    PDO::ATTR_PERSISTENT => true
+));
+
+$pdo = new PDO('mysql:host=localhost;dbname=thanksdad', "root", "girkinLOH1", array(
+    PDO::ATTR_PERSISTENT => true
+));
+*/
+
+$pdo = new PDO('mysql:host=188.166.55.191;dbname=yozh', "root", "girkinLOH1", array(
+  //  PDO::ATTR_PERSISTENT => true,
+//	PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
+));
+
+try
+{
+$secret_key = '56rZjlj9S2O0aUVE';
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+
+    print "Error!: " . $e->getMessage() . "<br/>";
+    die();
+}
+?>
---
+++ backendmysql/yozh_submitscore.php
@@ -0,0 +1,28 @@
+﻿<?php
+header('Content-type: text/json; charset=UTF-8');
+require_once 'yozh_connection.php';
+
+$score = $_POST['score'];
+$name = $_POST['name'];
+
+$plscript = "SELECT id, score FROM yozh.tb_players WHERE name = ".$pdo->quote($name);
+$statement = $pdo->prepare($plscript);
+$statement->execute();
+$res = $statement->fetchAll(PDO::FETCH_ASSOC);
+$player = $res[0];
+	if ($player != null) {
+		if ($score > $player['score'])
+		{
+			$wholequery = "UPDATE yozh.tb_players set score = ".$pdo->quote($score)." where name = ".$pdo->quote($name);
+			echo $wholequery;
+			$statement = $pdo->prepare($wholequery);
+			$statement->execute();
+		}
+	} else 
+	{
+		$plscript = "INSERT INTO yozh.tb_players (score, name) VALUES (".$pdo->quote($score).",".$pdo->quote($name).");";
+		echo $plscript;
+		$statement = $pdo->prepare($plscript);
+		$statement->execute();
+	}
+?>
